e5aa0c8b4eab2abbb3f5ec1568bc208c10270548
YejinMoon0916 <moonyejin60104@gmail.com>
2022-05-28 12:26:27 +0900

    test

---
+++ py06.py
@@ -0,0 +1,14 @@
+# input
+num1 = input("문제1> 3+3 = ")
+num2 = input("문제2> 5-2 = ")
+num3 = input("문제3> 5*2 = ")
+num4 = input("문제4> 10/2 = ")
+num5 = input("문제5> 10-3*3 = ")
+
+# output
+print()
+print("1번은", num1)
+print("2번은", num2)
+print("3번은", num3)
+print("4번은", num4)
+print("5번은", num5)
